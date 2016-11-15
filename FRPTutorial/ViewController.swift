b97fe32f05a4f97a95478c99a62502ed7261a0fe
Julia <juliageist23@gmail.com>
2016-11-14 23:28:39 -0800

    starter project -- without FRP

--- FRPTutorial/ViewController.swift
+++ FRPTutorial/ViewController.swift
@@ -7,21 +7,42 @@
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String = "Julia"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func changeNameButtonTapped(_ sender: AnyObject) {
+        print("changeNameButtonTapped")
+        showAlertController()
+    
+    }
+    
+    
+    func showAlertController() {
+        let title = "Update Name"
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        let okAction = UIAlertAction(title: "OK", style: .default) { action in
+            if let textInput = alert.textFields?.first?.text {
+                self.name = textInput
+                self.nameLabel.text = self.name
+            }
+        }
+        alert.addTextField()
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
